d712465f7ef4d5bff1a3c4478a7753a366886d91
Stew <ghost.pizza@gmail.com>
2025-09-20 11:33:42 +0200

    delete frontend

--- frontend/app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<NuxtLayout>
-<NuxtPage />
-<!-- Toaster globale -->
-    <Toaster />
-</NuxtLayout>
-</template>
--- frontend/middleware/auth.client.js
+++
@@ -1,8 +0,0 @@
-// Esegue solo sul client (nessun problema con localStorage/SSR)
-export default defineNuxtRouteMiddleware(() => {
-  const auth = useAuth()
-  auth.load() // recupera token da localStorage se presente
-  if (!auth.isAuthenticated.value) {
-    return navigateTo('/login')
-  }
-})
