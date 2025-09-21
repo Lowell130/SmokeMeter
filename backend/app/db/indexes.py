f544abe8ebbdccfc7e3e47ee7080dbda465f4bb6
Stew <ghost.pizza@gmail.com>
2025-09-21 14:13:01 +0200

    delted

--- backend/app/db/indexes.py
+++
@@ -1,4 +0,0 @@
-async def ensure_indexes(db):
-    await db.users.create_index("email", unique=True)
-    await db.logs.create_index([("user_id", 1), ("ts", -1)])
-    await db.metrics_daily.create_index([("user_id", 1), ("date", -1)], unique=True)
