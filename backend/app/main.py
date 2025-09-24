c5eb7b63bcfe4ef9d2b4716c639637e5f1f6f5e5
Stew130 <ghost.pizza@gmail.com>
2025-09-24 16:39:26 +0200

    go

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Smoke Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.CORS_ORIGINS],
+    allow_origins=[settings.CORS_ORIGINS, "https://smokio.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
